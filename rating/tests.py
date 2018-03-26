de2c786310f86c78fd036578aaa27db179e04b02
Simon Bao <Simonbao.sb@gmail.com>
2018-03-26 12:19:17 +0100

    Test Pass - Homepage Set server to host index page. And fix test location.

--- rating/tests.py
+++ rating/tests.py
@@ -7,3 +7,9 @@ class EntryModelTest(TestCase):
     def test_string_representation(self):
         entry = Entry(rating='5')
         self.assertEqual(str(entry), entry.rating)
+
+class WebpageTest(TestCase):
+
+    def test_homepage(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# Imports templates defined in settings.py
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'index.html'
+    
