f3dd57f5d641cf9302fd5420c94ee1c0f82d5412
Simon Bao <Simonbao.sb@gmail.com>
2018-03-26 14:36:51 +0100

    Update dependencies

--- rating/tests.py
+++ rating/tests.py
@@ -13,3 +13,13 @@ class WebpageTest(TestCase):
     def test_homepage(self):
         res = self.client.get('/')
         self.assertEqual(res.status_code, 200)
+
+    def test_review_one(self):
+        Entry.objects.create(rating='5')
+        res = self.client.get('/')
+        self.assertContains(res, '5')
+
+    def test_review_two(self):
+        Entry.objects.create(rating='2')
+        res = self.client.get('/')
+        self.assertContains(res, '2')
