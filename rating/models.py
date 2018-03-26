04c95135e4b9bc35ab8ece2179a772c760ab47c1
Simon Bao <Simonbao.sb@gmail.com>
2018-03-26 11:43:11 +0100

    Test Pass - Checking if correct rating is created.

--- rating/models.py
+++ rating/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Entry(models.Model):
     rating = models.CharField(max_length=1)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.rating
