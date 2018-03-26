95005a8fc3571dc1577198a8c5be3da8803065dd
Simon Bao <Simonbao.sb@gmail.com>
2018-03-26 11:47:40 +0100

    Test Fail - Visit Homepage

--- rating/models.py
+++ rating/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class WebpageTest(self):
+    res = self.client.get('/')
+    self.assertEqual(res.status_code, 200)
+
 class Entry(models.Model):
     rating = models.CharField(max_length=1)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
