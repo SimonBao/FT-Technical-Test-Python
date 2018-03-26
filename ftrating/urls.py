598fd8c535bf36a3b3f4dfe5fbd8b21e380f5126
Simon Bao <Simonbao.sb@gmail.com>
2018-03-26 14:58:50 +0100

    Change imports url

--- ftrating/urls.py
+++ ftrating/urls.py
@@ -1,11 +1,18 @@
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from rating import views
 # import all model views
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ftrating.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'ftrating.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#     url(r'^$', views.HomeView.as_view(), name="index"),
+#     url(r'^admin/', include(admin.site.urls)),
+# )
+
+
+urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name="index"),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
